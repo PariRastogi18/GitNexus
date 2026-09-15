d557d91bc67bb3586a24985a2967f1748cc61b87
Pari Rastogi <214537837+PariRastogi18@users.noreply.github.com>
2026-09-15 23:51:12 +0530

    complete context

--- frontend/src/components/context/AuthContext.js
+++ frontend/src/components/context/AuthContext.js
@@ -7,7 +7,9 @@ import {
   useState,
 } from "react";
 
-export default function AuthContext() {
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
   const BACKEND_URL = import.meta.env.BACKEND_URL;
   const [isAuthenticate, setIsAuthenticate] = useState(false);
   const [accessToken, setAccessToken] = useState(null);
@@ -28,34 +30,60 @@ export default function AuthContext() {
         throw new Error("Session expired!");
       }
 
-      const {accessToken} = await refreshRespond.json();
+      const { accessToken } = await refreshRespond.json();
       setAccessToken(accessToken);
       const response = await fetch(`${BACKEND_URL}/getMe`, {
-        method:"GET",
-        headers:{
-            Application:`Bearer ${accessToken}`
+        method: "GET",
+        headers: {
+          Application: `Bearer ${accessToken}`,
         },
-        credentials:"include"
+        credentials: "include",
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log("Verified user: ",data);
+        console.log("Verified user: ", data);
         setUser(data.user || data);
         isAuthenticate(true);
-      }else{
+      } else {
         setAccessToken(null);
-        isAuthenticate(false)
+        isAuthenticate(false);
         setUser(null);
       }
     } catch (error) {
-        setAccessToken(null);
-        isAuthenticate(false)
-        setUser(null);
-        throw new Error("Error: ", error.message);
-    }finally{
-        setLoading(false);
+      setAccessToken(null);
+      isAuthenticate(false);
+      setUser(null);
+      throw new Error("Error: ", error.message);
+    } finally {
+      setLoading(false);
     }
   }, []);
-  return <></>;
+
+  const login = useCallback(async (accessToken, user) => {
+    setAccessToken(accessToken);
+    setUser(user);
+    setIsAuthenticate(true);
+  }, []);
+
+  const logout = useCallback(async () => {
+    setAccessToken(null);
+    setIsAuthenticate(false);
+    setUser(null);
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticate, user, accessToken, loading, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be use within AuthProvider!");
+  }
+  return context;
 }
